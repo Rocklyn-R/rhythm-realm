e57d502cca3e7f2d6846e6eb4abb3aeeab6afab9
Rocklyn-R <antica.rusi@gmail.com>
2024-05-24 13:07:29 +0200

    Create image gallery for product

--- views/src/api/products.tsx
+++ views/src/api/products.tsx
@@ -13,13 +13,22 @@ export const getProducts = async (subcategory: string) => {
 
 export const getSelectedProduct = async (name: string, variant: string) => {
     try {
-        console.log(name);
-        console.log(variant);
         const response = await fetch(`http://localhost:4000/products/selected-product?name=${name}&variant=${variant}`);
         const data = await response.json();
-        console.log(data.selectedProduct)
         if (response.ok) {
             return data.selectedProduct[0];
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getAllVariants = async (id: number) => {
+    try {
+        const response = await fetch(`http://localhost:4000/products/variants?id=${id}`);
+        const data = await response.json();
+        if (response.ok) {
+            return data.variants;
         }
     } catch (error) {
         throw error;
--- views/src/features/Item/ImageGallery/ImageGallery.tsx
+++ views/src/features/Item/ImageGallery/ImageGallery.tsx
@@ -1,12 +1,78 @@
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux"
 import { selectSelectedProduct } from "../../../redux-store/ProductsSlice"
+import { ChevronLeftCircle, ChevronRightCircle, CircleDot, Circle } from "lucide-react";
 
 export const ImageGallery = () => {
     const selectedProduct = useSelector(selectSelectedProduct);
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const galleryImages = [selectedProduct.image1, selectedProduct.image2, selectedProduct.image3];
+
+
+    const showPrevImage = () => {
+        setImageIndex(index => {
+            if (index === 0) return index;
+            return index - 1;
+        })
+    }
+
+    const showNextImage = () => {
+        setImageIndex(index => {
+            if (index === galleryImages.length - 1) return index;
+            return index + 1;
+        })
+    }
 
     return (
-        <div className="w-1/2">
-            <img src={selectedProduct.image1} width="600" />
+
+        <div className='m-4 bg-white w-1/2 ml-10 h-full flex flex-col items-center relative'>
+            
+            <div className='w-full flex flex-row overflow-hidden pb-8'>
+
+                {galleryImages.map(url => (
+                    <img
+                        key={url}
+                        src={url}
+                        className="w-full h-full flex-shrink-0 flex-grow-0"
+                        style={{
+                            translate: `${-100 * imageIndex}%`,
+                            transition: 'translate 300ms ease-in-out'
+                        }}
+                    />
+                ))}
+            </div>
+            <div className='self-start'>
+                <button
+                    className={`${imageIndex === 0 ? 'gallery-gray-btn' : 'gallery-arrow-btn'} block absolute top-0 bottom-0 cursor-pointer left-0 p-1 bg-transparent`}
+                    onClick={showPrevImage}
+                >
+                    <ChevronLeftCircle
+                    />
+                </button>
+                <button
+                    className={`${imageIndex === galleryImages.length - 1 ? "gallery-gray-btn" : 'gallery-arrow-btn'} block absolute top-0 bottom-0 cursor-pointer right-0 p-1 bg-transparent`}
+                    onClick={showNextImage}
+                >
+                    <ChevronRightCircle
+                    />
+                </button>
+            </div>
+
+            <div className='flex'>
+                {galleryImages.map((src, index) => (
+                    <button
+                        className={`${imageIndex === index ? 'border-red-800 border-2' : 'border-gray-400 border'} w-16 mx-2 rounded-sm cursor-pointer block hover:scale-125 focus-visible:scale-125 transition duration-100 ease-in-out`}
+                        onClick={() => setImageIndex(index)}
+                    >
+                        <img
+                            src={src}
+                        />
+                    </button>
+                ))}
+            </div>
+
         </div>
+
     )
 }
