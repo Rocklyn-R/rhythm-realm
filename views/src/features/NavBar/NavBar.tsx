7fff856d41f8060f733a719b49930c82e9e156ce
Rocklyn-R <antica.rusi@gmail.com>
2025-02-08 15:08:57 +0100

    Add Loading to Subcategories

--- views/src/features/NavBar/NavBar.tsx
+++ views/src/features/NavBar/NavBar.tsx
@@ -1,17 +1,21 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom"
+import { setSubcategories } from "../../redux-store/FiltersSlice";
 
 export const NavBar = () => {
+
+    const dispatch = useDispatch();
     return (
         <div className="hidden sm:block">
             <ul className="flex justify-around p-2 bg-gray-900 text-gray-100 text-lg">
-                <li><Link to="/Guitars" className="hover:underline">Guitars</Link></li>
-                <li><Link to="/Basses" className="hover:underline">Basses</Link></li>
-                <li><Link to="/Drums" className="hover:underline">Drums</Link></li>
-                <li><Link to="/Keys" className="hover:underline">Keys</Link></li>
-                <li><Link to="/Strings" className="hover:underline">Strings</Link></li>
-                <li><Link to="/Winds" className="hover:underline">Winds</Link></li>
-                <li><Link to="/Audio" className="hover:underline">Audio</Link></li>
-                <li><Link to="/Accessories" className="hover:underline">Accessories</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Guitars" className="hover:underline">Guitars</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Basses" className="hover:underline">Basses</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Drums" className="hover:underline">Drums</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Keys" className="hover:underline">Keys</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Strings" className="hover:underline">Strings</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Winds" className="hover:underline">Winds</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Audio" className="hover:underline">Audio</Link></li>
+                <li><Link onClick={() => dispatch(setSubcategories([]))} to="/Accessories" className="hover:underline">Accessories</Link></li>
             </ul>
         </div>
     )
--- views/src/features/Subcategories/Subcategories.tsx
+++ views/src/features/Subcategories/Subcategories.tsx
@@ -6,6 +6,7 @@ import { getSubcategories } from "../../api/categories"
 import { useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 import { setLoadingProducts } from "../../redux-store/ProductsSlice"
+import { Loading } from "../Loading/Loading"
 
 
 export interface BreadcrumbPaths {
@@ -17,39 +18,38 @@ export interface BreadcrumbPaths {
 export const Subcategories: React.FC = () => {
   const { categoryName } = useParams<{ categoryName: string }>()
   const allCategories = useSelector(selectCategories);
-  const [id, setId] = useState(allCategories.find(item => item.name === categoryName)!.id);
   const dispatch = useDispatch();
   const subcategories = useSelector(selectSubcategories);
-
+  const [loading, setLoading] = useState(true);
   const categoryNameCapitalized = categoryName
     ? categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
     : '';
 
-  useEffect(() => {
-    if (allCategories) {
-      setId(allCategories.find(item => item.name === categoryName)!.id)
-    }
-  }, [allCategories, categoryName, id])
 
   useEffect(() => {
-    dispatch(setSubcategories([]));
+    setLoading(true);
     const fetchSubcategories = async () => {
+      const id = allCategories.find(item => item.name === categoryName)!.id;
       const subcategoryData = await getSubcategories(id);
       if (subcategoryData) {
         dispatch(setSubcategories(subcategoryData));
+        setLoading(false);
       }
     }
-    if (allCategories && id) {
+    if (allCategories) {
+      
       fetchSubcategories();
     }
 
-  }, [dispatch, categoryName, allCategories, id]);
+  }, [dispatch, categoryName, allCategories, setLoading]);
+  
 
 
   return (
     <div className="flex flex-col items-center mb-14">
       <h2 className="text-center text-2xl font-bold mb-6">{categoryNameCapitalized}:</h2>
-      <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-8 w-fit max-w-7xl">
+      {loading ? <Loading/> : (
+            <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 px-8 w-fit max-w-7xl">
         {categoryName && subcategories.map((subcategory, index) => (
           <Link
           onClick={() => dispatch(setLoadingProducts(true))}
@@ -68,6 +68,8 @@ export const Subcategories: React.FC = () => {
           </Link>
         ))}
       </div>
+      )}
+  
     </div>
   )
 }
